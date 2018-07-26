093b577ebc405eea7eed6ca86e9a12b33e83cf10
Ivan Morett <luisivanmorett@gmail.com>
2018-07-26 15:40:18 -0700

    set date push notifs

--- cloud/main.js
+++ cloud/main.js
@@ -8,6 +8,9 @@ Parse.Cloud.define("setSleepReminder", function(req, res){
   console.log("Starting the push notification");
   let query = new Parse.Query(Parse.User);
   console.log("User is: " + req.params.userId);
+  // Set the date that the notification is supossed to arrive
+  let date = new Date();
+  date.setMinutes(date.getMinutes()+5);
   query.equalTo("objectId", req.params.userId);
   query.limit(1);
   query.find().then(function(results){
@@ -17,6 +20,7 @@ Parse.Cloud.define("setSleepReminder", function(req, res){
       pushQuery.equalTo('user', user);
       Parse.Push.send({
         where: pushQuery,
+        push_time: date,
         data: {
           alert: "Hey you're + " + user.get("username"),
           badge: 1,
