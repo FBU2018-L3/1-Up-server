35ce852ccb35cc541e21222c0e8dfe8257359ae4
Ivan Morett <luisivanmorett@gmail.com>
2018-07-26 15:15:20 -0700

    Moved the notification to function

--- cloud/main.js
+++ cloud/main.js
@@ -5,29 +5,10 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.afterSave(Parse.User, function(req){
-    logger.info("Started");
     const user = req.object;
-    logger.info("USER is: " + user.get("username"));
-    logger.info("request: " + JSON.stringify(req));
 
-    var pushQuery = new Parse.Query(Parse.Installation);
-    pushQuery.equalTo('user', user);
-    Parse.Push.send({
-      where: pushQuery,
-      data: {
-        alert: "Hey you're + " + user.get("username"),
-        badge: 1,
-        sound: 'default'
-      }
-    }, {
-      success: function() {
-        console.log('##### PUSH OK');
-      },
-      error: function(error) {
-        console.log('##### PUSH ERROR');
-      },
-      useMasterKey: true
-    });
+    
+    
     // let query = new Parse.Query(Parse.User);
     // query.equalTo("objectId", userId);
     // query.limit(1);
@@ -46,5 +27,38 @@ Parse.Cloud.afterSave(Parse.User, function(req){
     // catch(f){
     //   console.error(error);
     // }
+});
+
+Parse.Cloud.define("setSleepReminder", function(req, res){
+  let query = new Parse.Query(Parse.User);
+  query.equalTo("objectId", req.params.userId);
+  query.limit(1);
+  query.find().then(function(results){
+    if(results.length>0){
+      const user = results[0];
+      var pushQuery = new Parse.Query(Parse.Installation);
+      pushQuery.equalTo('user', user);
+      Parse.Push.send({
+        where: pushQuery,
+        data: {
+          alert: "Hey you're + " + user.get("username"),
+          badge: 1,
+          sound: 'default'
+        }
+      }, {
+        success: function() {
+          console.log('##### PUSH OK');
+          response.success("ok");
+        },
+        error: function(error) {
+          console.error('##### PUSH ERROR');
+          response.error("error");
+        },
+        useMasterKey: true
+      });
+    }
+  }, function(error){
+    console.error("ERROR getting user");
+  })
 });
 
