3bbe6f9e5d9fe8948a001517b777656c3380500d
Ivan Morett <luisivanmorett@gmail.com>
2018-07-26 12:21:42 -0700

    error control

--- cloud/main.js
+++ cloud/main.js
@@ -5,7 +5,11 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.afterSave(Parse.User, function(request){
     let user = request.get("user");
-    console.log(user.get("isAsleep"))
-    Response.success("");
+    try{
+      console.log(user.get("isAsleep"))
+    }
+    catch(error){
+      console.error(error);
+    }
 });
 
