98c83039f3543a16f80bc34b418f5b4172e337ba
Ivan Morett <luisivanmorett@gmail.com>
2018-07-26 12:31:32 -0700

    Obtaining user

--- cloud/main.js
+++ cloud/main.js
@@ -4,12 +4,25 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.afterSave(Parse.User, function(request, response){
-    let user = request.object;
-    try{
-      console.log(user.get("isAsleep"))
-    }
-    catch(f){
-      console.error(error);
-    }
+    Console.log("After saved started");
+    const userId = request.object.id;
+    let query = new Parse.Query(Parse.User);
+    query.equalTo("objectId", userId);
+    query.limit(1);
+    query.find().then(function(results){
+      if(results.length>0){
+        var user = results[0];
+        console.log(user.get("isAsleep"));
+      }
+    }, function(error){
+      console.error("ERROR getting user");
+    })
+    // Console.log("User obtained succesfully");
+    // try{
+    //   console.log(user.get("isAsleep"))
+    // }
+    // catch(f){
+    //   console.error(error);
+    // }
 });
 
