cee49cc6e4b0abafb9fb7594d094f1af07c60649
Ivan Morett <luisivanmorett@gmail.com>
2018-07-26 12:17:34 -0700

    parse cloud

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,11 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+    res.success('Hi');
 });
+
+Parse.Cloud.afterSave(Parse.User, function(request){
+    let user = request.get("user");
+    console.log(user.get("isAsleep"))
+    Response.success("");
+});
+
