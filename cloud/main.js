6d4a621258a1f9e0b7e021d5dc086fcb075efdf4
Ivan Morett <luisivanmorett@gmail.com>
2018-07-26 13:59:13 -0700

    Testing notifs in parse cloud

--- cloud/main.js
+++ cloud/main.js
@@ -8,6 +8,25 @@ Parse.Cloud.afterSave(Parse.User, function(req){
     logger.info("Started");
     const user = req.object;
     logger.info("USER is: " + user.get("username"))
+
+    var query = new Parse.Query(Parse.Installation);
+
+    Parse.Push.send({
+      where: query,
+      data: {
+        alert: 'Test',
+        badge: 1,
+        sound: 'default'
+      }
+    }, {
+      success: function() {
+        console.log('##### PUSH OK');
+      },
+      error: function(error) {
+        console.log('##### PUSH ERROR');
+      },
+      useMasterKey: true
+    });
     // let query = new Parse.Query(Parse.User);
     // query.equalTo("objectId", userId);
     // query.limit(1);
